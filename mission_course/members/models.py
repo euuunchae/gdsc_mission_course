1cd920b990b3b9424b3ebd823074b0f1dda2aa0c
GitHub 핸들명 <GitHub >
2024-12-01 11:38:23 +0900

    restframework 설치

--- mission_course/members/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Member(models.Model):
-    name = models.TextField()
-    email = models.TextField()
-    birth = models.DateTimeField()
-    join_date = models.DateTimeField()
