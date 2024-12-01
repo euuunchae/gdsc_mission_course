775ea9849ffe0b8f708355d779befab3d650d79b
GitHub 핸들명 <GitHub >
2024-12-01 11:20:47 +0900

    레포지토리 다시 열기

--- mission_course/members/models.py
+++ mission_course/members/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Member(models.Model):
-    name = models.TextField
+    name = models.TextField()
     email = models.TextField()
-    birth = models.DateTimeField
+    birth = models.DateTimeField()
     join_date = models.DateTimeField()
